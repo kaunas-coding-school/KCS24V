68523884b74951c8ec116ac8347e39e0714a589e
Tautvydas Dulskis <tautvydas@dulskis.eu>
2020-04-03 18:30:00 +0300

    Funkcijos

--- public_html/js/jsUzduotis.js
+++ public_html/js/jsUzduotis.js
@@ -1,18 +1,3 @@
-let kiekis = document.querySelectorAll('img').length;
-let kiekText = 'Šiame puslapyje yra ' + kiekis + ' paveiksleliai';
-
-console.log(kiekText);
-
-document.getElementById('uuid').innerHTML = kiekText;
-
-let fotos = document.querySelectorAll('img:nth-child(2n)');
-
-for (let i = 0; i < fotos.length; ++i) {
-    elem = fotos[i];
-    elem.setAttribute("alt", "Testas");
-}
-
-let uuid = document.getElementById('uuid');
 let style = {
     "fontSize": "24px",
     "backgroundColor": "green",
@@ -20,19 +5,78 @@ let style = {
     "height":"200px",
 };
 
-// Pirmas budas
-// uuid.style.fontSize = '24px';
-// uuid.style.backgroundColor = 'green';
-// uuid.style.width = '200px';
-// uuid.style.height = '200px';
+function skaiciuotiElemKieki(elem) {
+    return document.querySelectorAll(elem).length;
+}
 
-// Antras budas
-// for( let key in style ){
-//     uuid.style[key] = style[key]
-// }
+function generuotiAtsakyma(kiekis) {
 
-// Trecias budas
-// Object.assign(uuid.style, style);
+    return 'Šiame puslapyje yra ' + kiekis + ' paveiksleliai';
+}
 
-// Ketvirtas budas
-uuid.setAttribute("class", "stilius2");
+function getFotos() {
+    return document.querySelectorAll('img:nth-child(2n)');
+}
+
+function keistiAltTeksta() {
+    for (let i = 0; i < fotos.length; ++i) {
+        elem = fotos[i];
+        elem.setAttribute("alt", "Testas");
+    }
+}
+
+function gautiElementa(id) {
+    return document.getElementById(id);
+}
+
+function ketvirtasBudas(){
+    // Ketvirtas budas
+    uuid.setAttribute("class", "stilius2");
+}
+
+function treciasBudas(){
+    // Trecias budas
+    Object.assign(uuid.style, style);
+}
+
+function antrasBudas(){
+    // Antras budas
+    for( let key in style ){
+        uuid.style[key] = style[key]
+    }
+}
+
+function pirmasBudas(){
+    // Pirmas budas
+    uuid.style.fontSize = '24px';
+    uuid.style.backgroundColor = 'green';
+    uuid.style.width = '200px';
+    uuid.style.height = '200px';
+}
+function spausdintiTeksta(kur, ka) {
+    kur.innerHTML = ka;
+}
+
+// ********************************************************************************************************
+// ********************************************************************************************************
+// ********************************************************************************************************
+
+let imgKiekis = skaiciuotiElemKieki('img');
+let kiekText = generuotiAtsakyma(imgKiekis);
+let fotos = getFotos();
+let uuid = gautiElementa('uuid');
+let uuid2 = gautiElementa('uuid2');
+
+keistiAltTeksta();
+// pirmasBudas();
+// antrasBudas();
+// treciasBudas();
+ketvirtasBudas();
+
+// ********************************************************************************************************
+// ********************************************************************************************************
+// ********************************************************************************************************
+
+console.log(kiekText);
+spausdintiTeksta(uuid, kiekText);
+spausdintiTeksta(uuid2, 'UUID 2: ' + kiekText);

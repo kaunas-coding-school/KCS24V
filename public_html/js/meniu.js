87b33efd0ba161c4acbbb4c6490ff77eafab2c6d
Tautvydas Dulskis <tautvydas@dulskis.eu>
2020-04-06 21:41:18 +0300

    Jquery + Meniu

--- public_html/js/meniu.js
+++ public_html/js/meniu.js
@@ -1,1 +1,17 @@
- // ????
+function openPage(pageName) {
+    $('nav > a').removeClass('active');
+    $('main > p').css('display', 'none');
+    $(pageName).css('display', 'block');
+    $('[href="'+pageName+'"]').addClass('active');
+}
+
+$('nav > a').click(function () {
+    let pageName = $(this).attr('href');
+    openPage(pageName);
+});
+
+
+let hash = $(location).attr('hash');
+openPage(hash);
+
+
